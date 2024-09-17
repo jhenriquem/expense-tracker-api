f622fa7053d29ca472177cd280df392ac75fb2a9
jhenriquem <machadojh11@gmail.com>
2024-09-17 20:40:36 -0300

    (sys categories) creation of routes to return the categories and add las

--- routes/accountRoutes.ts
+++ routes/accountRoutes.ts
@@ -2,11 +2,26 @@ import { Router } from "express";
 import timeRoute from "./subroutes/timeRoutes";
 import categoryRoute from "./subroutes/categoryRoutes";
 import expensesRoute from "./subroutes/expensesRoutes";
+import userModel from "../models/userModel";
 
 const accountRoute = Router()
 
-accountRoute.get("/", (req, res) => {
-  res.json(req.originalUrl)
+accountRoute.get("/", async (req, res) => {
+  try {
+    const { userId } = req.body
+    const userBase = await userModel.findOne({ _id: userId }, { credentials: 0, _id: 0, __v: 0 })
+    return res.status(200).json({
+      statusMessage: "Success",
+      data: userBase
+    })
+  } catch (err: any) {
+    return res.status(500).json({
+      statusmessage: "Error when trying to return the user data",
+      data: {
+        error: err.message
+      }
+    })
+  }
 })
 // Responsible for times related filters
 accountRoute.use("/filters/time", timeRoute)
@@ -20,6 +35,6 @@ accountRoute.use("/filters/category/:categoryName", (req, res) => {
 accountRoute.use("/expenses", expensesRoute)
 
 // Responsible for adding and returning the categorys
-accountRoute.use("/category", categoryRoute)
+accountRoute.use("/categories", categoryRoute)
 
 export default accountRoute
--- routes/subroutes/categoryRoutes.ts
+++ routes/subroutes/categoryRoutes.ts
@@ -1,15 +1,49 @@
 import { Router } from "express";
+import userModel from "../../models/userModel";
 
 const categoryRoute = Router()
 
 // Route responsible for returning all categorys
-categoryRoute.get("/", (req, res) => {
-  res.json(req.url)
+categoryRoute.get("/", async (req, res) => {
+  try {
+    const { userId } = req.body
+    const userBase = await userModel.findById(userId)
+    return res.status(200).json({ statusMessage: "Success", data: userBase?.categories })
+  } catch (err: any) {
+    return res.status(500).json({
+      statusmessage: "error when trying to return the category list of the user",
+      data: {
+        error: err.message
+      }
+    })
+  }
 })
 
 // Route responsible for adding new category
-categoryRoute.post("/", (req, res) => {
-  res.json(req.url)
+categoryRoute.post("/", async (req, res) => {
+  try {
+    const { newCategory, userId } = req.body
+    const isValid = newCategory && newCategory !== ""
+
+    if (!isValid) {
+      return res.status(400).json({
+        statusMessage: "Invalid or non-existent new category"
+      })
+    }
+
+    const userBase = await userModel.findById(userId)
+    userBase?.categories.push(newCategory)
+    userBase?.save()
+
+    return res.status(200).json({ statusMessage: "Success" })
+  } catch (err: any) {
+    return res.status(500).json({
+      statusmessage: "error when trying to adding a new category in the list of the user",
+      data: {
+        error: err.message
+      }
+    })
+  }
 })
 
 export default categoryRoute
